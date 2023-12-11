18b61fb83f1a3f7c8040be6074c377b0f01d5ded
David <ddlukula21@coe.edu>
2023-12-11 13:29:00 -0600

    Lots of changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mysql.connector import connect, Error
 import csv
 import numpy as np
+import random
 
 def createSchedule(mydb):
     mycursor = mydb.cursor();
@@ -8,6 +9,7 @@ def createSchedule(mydb):
     DROP TABLE if exists Schedule;
     CREATE Table Schedule
     (
+    courseID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     title VARCHAR(50),
     instructor VARCHAR(20),
     days VARCHAR(3),
@@ -33,8 +35,8 @@ def createStudent(mydb):
     DROP TABLE if exists STUDENT;
     CREATE TABLE STUDENT
     (
-    studentID INT PRIMARY KEY NOT NULL,
-    studentNumber INT NOT NULL,
+    studentID INT,
+    studentNumber INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     fname VARCHAR(20),
     lname VARCHAR(20),
     classYear  ENUM('Fr', 'So', 'Jr', 'Sr'),
@@ -80,7 +82,7 @@ def insertNewClass(mydb, title, instructor, days, begtime, endtime, bldgRoom, cr
     term = "Fall"
     mycursor = mydb.cursor();
     query = """INSERT INTO Schedule 
-    (title, instructor, days, begtime, endtime,bldgRoom,credits,year,term)
+    (title,instructor, days, begtime, endtime,bldgRoom,credits,year,term)
     VALUES"""
     values = title, instructor, days, begtime, endtime, bldgRoom, credits,year,term
     query += str(values)
@@ -93,7 +95,44 @@ def insertNewClass(mydb, title, instructor, days, begtime, endtime, bldgRoom, cr
     except Error as e:
         print(e)
 
+def insertNewStudent(mydb, studentID, fname, lname, classYear, Major1, Major2, Minor1,Advisor):
+    mycursor = mydb.cursor();
+    query = """Insert into Student
+       (studentID,fname ,lname ,classYear,Major1,Major2 ,Minor1 ,Advisor)
+        values """
+    values = studentID, fname, lname, classYear, Major1, Major2, Minor1,Advisor
+    query += str(values)
+    query += ";"
+    print(query)
+    try:
+        mycursor.execute(query)
+        mydb.commit()
+        for result in mycursor.fetchall():
+            print(result)
+    except Error as e:
+        print(e)
 
+def populateEnrollment(mydb):
+    enrollStudent(mydb, "1242", "164")
+    enrollStudent(mydb, "1242", "2")
+    enrollStudent(mydb, "1242", "22")
+    enrollStudent(mydb, "1242", "39")
+    enrollStudent(mydb, "1252", "164")
+    enrollStudent(mydb, "1252", "68")
+    enrollStudent(mydb, "1252", "86")
+    enrollStudent(mydb, "1252", "117")
+    enrollStudent(mydb, "1262", "164")
+    enrollStudent(mydb, "1262", "115")
+    enrollStudent(mydb, "1262", "112")
+    enrollStudent(mydb, "1262", "138")
+    enrollStudent(mydb, "1272", "164")
+    enrollStudent(mydb, "1272", "237")
+    enrollStudent(mydb, "1272", "277")
+    enrollStudent(mydb, "1272", "326")
+    enrollStudent(mydb, "1282", "164")
+    enrollStudent(mydb, "1282", "237")
+    enrollStudent(mydb, "1282", "277")
+    enrollStudent(mydb, "1282", "326")
 def courseDept(mydb,dept):
     mycursor = mydb.cursor();
     leng = len(dept)
@@ -124,6 +163,32 @@ def courseTime(mydb,starttime):
     except Error as e:
         print(e)
 
+def DCPlookup(mydb):
+    mycursor = mydb.cursor();
+
+    query = """
+    select * 
+    from Schedule 
+    where SUBSTRING(title,8,1) = "7" ;
+    
+    select * 
+    from Schedule 
+    where SUBSTRING(title,8,1) = "6" ;
+
+    select * 
+    from Schedule 
+    where SUBSTRING(title,8,1) = "8" ;
+    
+    """
+
+
+    try:
+        mycursor.execute(query);
+        for result in mycursor.fetchall():
+            print(result)
+    except Error as e:
+        print(e)
+
 
 def profLookup(mydb,profName):
     mycursor = mydb.cursor();
@@ -137,41 +202,212 @@ def profLookup(mydb,profName):
     except Error as e:
         print(e)
 
-def generateStudents(mydb):
+
+
+
+
+def getStudentID(mydb,lname):
     mycursor = mydb.cursor();
     query = """
-    Insert into Student
-        (studentID ,studentNumber ,fname ,lname ,classYear,Major1,Major2 ,Minor1 ,Advisor)
-        values
-        ('1242','1',"David","Lukula","Jr","CS","","DS","Hughes"),
-        ("1252","2","Chris","Baker","Jr","CS","","DS","Hughes"),
-        ("1262","3","Cael","Schmitt","Sr","CS","","DS","Hughes"),
-        ('1272','4',"Stephen","Hooghs","Fr","CS","PSY","ANT","Hughes"),
-        ('1282','5',"John","Doe","So","CS","PHY","DS","Hughes");
-        
-        
-    Insert into Enrollment
-	(studentID,courseID,status,classSize)
-	values
-    ('1242',"1","Active",100),
-    ('1242',"2","Active",100),
-    ('1242',"3","Active",100),
-    ('1242',"4","Active",100),
-    ("1252","5","Active",100),
-    ("1252","2","Active",100),
-    ("1252","3","Active",100),
-    ("1252","6","Active",100),
-    ("1262","1","Active",100),
-    ("1262","3","Active",100),
-	("1262","5","Active",100),
-    ("1262","7","Active",100),
-	("1272","8","Active",100),
-	("1272","2","Active",100),
-	("1272","6","Active",100),
-	("1272","3","Active",100);
-    
-    
+    select studentID
+    from Student
+    where lname = 
     """
+    query += "\"" + lname + "\""
+    print(query)
+    try:
+        mycursor.execute(query);
+        for result in mycursor.fetchall():
+            print(result)
+    except Error as e:
+        print(e)
+
+def getCourseID(mydb,title):
+    mycursor = mydb.cursor();
+    query =  """
+    select courseID
+    from schedule
+    where title =
+    """
+    query += "\"" + title + "\""
+    print(query)
+    try:
+        mycursor.execute(query);
+        for result in mycursor.fetchall():
+            print((result[0]))
+    except Error as e:
+        print(e)
+
+def aliasgetCourseID(mydb,title):
+    mycursor = mydb.cursor();
+    m = ""
+    query =  """
+    select courseID
+    from schedule
+    where title like 
+    """
+    if title[-1] == "W":
+        title = title[:-4]
+    query += "\"" + title + "%\""
+    #print(query)
+    try:
+        mycursor.execute(query);
+        for result in mycursor.fetchall():
+            return (result[0])
+    except Error as e:
+        print(e)
+
+def enrollStudent(mydb,sID,cID):
+    mycursor = mydb.cursor();
+    status = "Active"
+    cSize = "100"
+    query = """
+    Insert into Enrollment
+	    (studentID,courseID,status,classSize)
+	    values
+	    (
+	    """
+    query += "\"" + sID + "\","
+    query += "\"" + cID + "\","
+    query += "\"" + status + "\","
+    query += "\"" + cSize + "\");"
+    print(query)
+    try:
+        mycursor.execute(query)
+        mydb.commit()
+        for result in mycursor.fetchall():
+            print(result)
+    except Error as e:
+        print(e)
+
+
+def anonymize(mydb):
+    aliases = {}
+    idnums = []
+    lname = []
+    fname = []
+    Class = []
+    major1 = []
+    major2 = []
+    minor = []
+    advisor = []
+    generateID = random.randint(10000000,19999999)
+
+    #print(generateID , "Generated")
+    with open('Registration.csv') as infile:
+        csvFile = csv.reader(infile)
+        header = next(csvFile)
+        for line in csvFile:
+            if (len(line[0]) == 8):
+                idnums.append(line[0])
+                lname.append(line[1])
+                fname.append(line[2])
+                Class.append(line[3])
+                major1.append(line[4])
+                major2.append(line[5])
+                minor.append(line[6])
+                advisor.append(line[7])
+
+
+        #print(idnums,lname,fname)
+        #generatename = random.randint(0, len(idnums) - 1)
+        for i in range (len(idnums)):
+            t = random.randint(10000000,19999999)
+            tt = lname[random.randint(0, len(idnums) - 1)]
+            ttt = fname[random.randint(0, len(idnums) - 1)]
+            if idnums[i] not in aliases:
+                aliases[idnums[i],lname[i],fname[i]] = t, tt, ttt
+               # insertNewStudent(mydb, t, ttt, tt, Class[i], major1[i], major2[i], minor[i], advisor[i])
+
+    return aliases
+
+def enrollAliases(mydb):
+    alises = anonymize(mydb)
+
+    with open('Registration.csv') as infile:
+        currentclass = ""
+        csvFile = csv.reader(infile)
+        header = next(csvFile)
+        currclass = (next(csvFile))
+        currentclass = currclass.pop()
+        currentclass = aliasgetCourseID(mydb,currentclass)
+        for line in csvFile:
+            if (len(line[0]) > 8 ):
+                currentclass = aliasgetCourseID(mydb,line[0])
+            else:
+                for key in alises:
+                    if key[0] == line[0]:
+                        enrollStudent(mydb,str(alises[key][0]),str(currentclass))
+
+
+
+def listStudent(mydb): #This Function shows you all your students with their ID numbers in alphabetical order by the lastname
+    mycursor = mydb.cursor();
+    query = """
+    select studentID,fname,lname 
+    from student
+    order by lname, fname;
+    """
+    #print(query)
+    try:
+        mycursor.execute(query);
+        for result in mycursor.fetchall():
+            print(result)
+    except Error as e:
+        print(e)
+
+def studentLookup(mydb,idnum): #This lets you look up and find all the students information by just by the id number
+    mycursor = mydb.cursor();
+    query = """
+        select *
+    from student
+    where studentID ="""
+    query += idnum
+    #print(query)
+    try:
+        mycursor.execute(query);
+        for result in mycursor.fetchall():
+            print(result)
+    except Error as e:
+        print(e)
+
+def instructorClasses(mydb,instructor): #This lets you find all the classes a instructor is teaching
+    mycursor = mydb.cursor();
+    query = """
+        select *
+    from schedule
+    where instructor =
+    """
+    query += "\"" + instructor + "\""
+    print(query)
+    try:
+        mycursor.execute(query);
+        for result in mycursor.fetchall():
+            print(result)
+    except Error as e:
+        print(e)
+
+
+
+
+#This function shows you the ID, first and last name of every student who is taking the class
+def takingClass(mydb,title):
+    mycursor = mydb.cursor();
+    query = """
+       select studentID
+from enrollment
+where courseID = 7
+       """
+    print(query)
+    try:
+        mycursor.execute(query);
+        for result in mycursor.fetchall():
+            print(result)
+    except Error as e:
+        print(e)
+
+
+
 
 def main():
     titles = []
@@ -211,16 +447,30 @@ def main():
             password="Coding12@",  # Replace with yours
             database="Project3"
         )
+        #instructorClasses(mydb,"S Hughes")
+        #studentLookup(mydb,"1242")
+        #listStudent(mydb)
+        #enrollAliases(mydb)
+        #aliasgetCourseID(mydb,"AAM  137  01   W")
+        #anonymize(mydb)
+        #populateEnrollment(mydb)
+        #insertNewStudent(mydb,'1242',"David","Lukula","Jr","CS","","DS","Hughes")
+        #insertNewStudent(mydb,"1252","Chris","Baker","Jr","CS","","DS","Hughes")
+        #insertNewStudent(mydb,"1262","Cael","Schmitt","Sr","CS","","DS","Hughes")
+        #insertNewStudent(mydb,'1272',"Stephen","Hooghs","Fr","CS","PSY","ANT","Hughes")
+        #insertNewStudent(mydb,'1282',"John","Doe","So","CS","PHY","DS","Hughes")
         #createEnrollment(mydb)
         #createStudent(mydb)
         #profLookup(mydb,"S Harmsen")
         #courseTime(mydb,"10:00 AM")
         #courseDept(mydb,"AAM")
+        #DCPlookup(mydb)
         #createSchedule(mydb)
         #for i in range(len(titles)):
-             #insertNewClass(mydb,titles[i],instructor[i],days[i],begtime[i],endtime[i],bldgRoom[i],creditz[i])
+            #insertNewClass(mydb,titles[i],instructor[i],days[i],begtime[i],endtime[i],bldgRoom[i],creditz[i])
              #myinfo = (titles[i], instructor[i], days[i], begtime[i], endtime[i], bldgRoom[i], creditz[i])
     except Error as e:
         print(e)
 
 main()
+
